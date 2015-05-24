abeeca31370ab21f9f0e0429173c18b9f74b3fc4
Freebien <freebien@gmail.com>
2015-05-24 12:31:19 +0200

    Change to relative files of savemaster

--- savemaster.sh
+++ savemaster.sh
@@ -7,8 +7,8 @@
 #
 ################
 
-[ -e /opt/savemaster/savemaster.cfg ] && . /opt/savemaster/savemaster.cfg || ( echo "Config file doesn't exist please create it." && exit 1 )
-[ -e /opt/savemaster/functions.sh ] && . /opt/savemaster/functions.sh || ( echo "Functions file doesn't exist." && exit 1 )
+[ -e savemaster.cfg ] && . savemaster.cfg || ( echo "Config file doesn't exist please create it." && exit 1 )
+[ -e functions.sh ] && . functions.sh || ( echo "Functions file doesn't exist." && exit 1 )
 
 [ -d $savepath ] || mkdir -p $savepath
 
